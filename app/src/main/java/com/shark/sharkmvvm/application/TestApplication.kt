d08bfe3ac849f65d8930c691c77dd007528f5f94
fujie <1243596620@qq.com>
2021-11-10 21:55:27 +0800

    XUI引入

--- app/src/main/java/com/shark/sharkmvvm/application/TestApplication.kt
+++ app/src/main/java/com/shark/sharkmvvm/application/TestApplication.kt
@@ -3,11 +3,16 @@ package com.shark.sharkmvvm.application
 import com.shark.mvvm.application.SharkApplication
 import com.shark.mvvm.config.HttpCode
 import com.shark.mvvm.config.HttpConfig
+import com.xuexiang.xui.XUI
 
 class TestApplication : SharkApplication() {
     override fun onCreate() {
         super.onCreate()
         HttpConfig.BASE_URL_WEATHER = "http://retailtest.qingyizhu.net/api/"
         HttpCode.CODE_SUCCESS = "200"
+
+        //设置默认字体为华文行楷
+//        XUI.initFontStyle("fonts/hwxk.ttf")
+
     }
 }
--- mvvm/src/main/java/com/shark/mvvm/activity/MvvmActivity.kt
+++ mvvm/src/main/java/com/shark/mvvm/activity/MvvmActivity.kt
@@ -1,5 +1,6 @@
 package com.shark.mvvm.activity
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
