16248321f045aacc6e43f2d1277b91278cfcc51b
bigGreenPeople <1243596620@qq.com>
2022-02-22 16:28:41 +0800

    扫描检测页面完成

--- tools/src/main/java/com/shark/tools/activity/ScanKeyCodeActivity.kt
+++ tools/src/main/java/com/shark/tools/activity/ScanKeyCodeActivity.kt
@@ -6,15 +6,17 @@ import androidx.lifecycle.MutableLiveData
 import com.shark.mvvm.activity.MvvmActivity
 import com.shark.mvvm.service.Service
 import com.shark.mvvm.utils.schedule
+import com.shark.mvvm.viewmodel.SharkViewModel
 import com.shark.tools.R
 import com.shark.tools.databinding.ActivityScanKeyCodeBinding
 import com.shark.tools.viewmodel.ScanKeyCodeViewModel
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 class ScanKeyCodeActivity : MvvmActivity() {
     lateinit var mDataBinding: ActivityScanKeyCodeBinding
 
-    @Service
+    @SharkViewModel
     lateinit var viewModel: ScanKeyCodeViewModel
 
     //这个用来存储按下并弹起的按键 key为ACTION_DOWN val为ACTION_UP
@@ -24,20 +26,43 @@ class ScanKeyCodeActivity : MvvmActivity() {
         initSharkActivity(R.layout.activity_scan_key_code)
     }
 
-
+    lateinit var timer: Timer
     override fun initView() {
-        schedule(period = 300) {
+        timer = schedule(period = 300) {
             checkScanKey()
         }
         mDataBinding.viewModel = viewModel
     }
 
-    fun confirm(view: View) {
+    fun add(view: View) {
+        reset(view)
+        timer = schedule(period = 300) {
+            checkScanKey()
+        }
+    }
 
+    fun next(view: View) {
+
+        finish()
+    }
+
+    fun confirm(view: View) {
+        val filter = keyDownUpMap.filter { keyDownUp -> keyDownUp.value != -1 }
+        if (filter.size != 1) return
+
+        timer.cancel()
+
+        val keyCode = filter.values.toMutableList()[0]
+        mDataBinding.tvMessage.text = "添加${keyCode}成功"
+
+        keyDownUpMap.clear()
+        viewModel.status.value = 3
     }
 
     fun reset(view: View) {
         keyDownUpMap.clear()
+        mDataBinding.tvMessage.text = "请按下扫描按键"
+        viewModel.status.value = 0
     }
 
     private fun checkScanKey() {
@@ -45,18 +70,21 @@ class ScanKeyCodeActivity : MvvmActivity() {
         if (filter.isEmpty()) {
             runOnUiThread {
                 mDataBinding.tvMessage.text = "请按下扫描按键"
+                viewModel.status.value = 0
             }
             return
         }
         if (filter.size > 1) {
             runOnUiThread {
                 mDataBinding.tvMessage.text = "检测到扫描按键有多个,无法确定扫描按键"
+                viewModel.status.value = 1
             }
             return
         }
 
         runOnUiThread {
             mDataBinding.tvMessage.text = "检测成功! 扫描按键为:${filter.values.toMutableList()[0]}"
+            viewModel.status.value = 2
         }
     }
 
