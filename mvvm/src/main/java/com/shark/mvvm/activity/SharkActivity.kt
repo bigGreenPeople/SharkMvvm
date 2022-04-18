06b20058575ac5845a0bfac1967545f3ceee2258
bigGreenPeople <1243596620@qq.com>
2022-04-18 12:24:41 +0800

    版本变化

--- mvvm/src/main/java/com/shark/mvvm/activity/SharkActivity.kt
+++ mvvm/src/main/java/com/shark/mvvm/activity/SharkActivity.kt
@@ -5,7 +5,6 @@ package com.shark.mvvm.activity
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 annotation class SharkActivity(
-    val value: String,       //路由跳转规则 此参数仅在组件化时起到作用
     val layoutId: Int = 0,          //布局id
     val back: Boolean = true,    //显示返回界面
     val title: String = ""    //标题
